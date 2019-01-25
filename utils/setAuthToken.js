cd14bf9e6d819fb171b9d178a700e4f22b413050
RC Maples <rcmaples@users.noreply.github.com>
2019-01-24 20:34:48 -0500

    Beginning of the end.

---
+++ utils/setAuthToken.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+module.exports = setAuthToken;

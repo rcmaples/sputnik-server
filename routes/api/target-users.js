521b7f448134eadee07777279284a2a3fe88fe91
RC Maples <rcmaples@users.noreply.github.com>
2019-01-27 12:17:57 -0500

    adjusted login form error handling and re-set target-users route.

--- routes/api/target-users.js
+++ routes/api/target-users.js
@@ -12,73 +12,48 @@ const User = require('../../models/User');
 const Repo = require('../../models/Repo');
 const Event = require('../../models/Event');
 const TargetUser = require('../../models/TargetUser');
-const setAuthToken = require('../../utils/setAuthToken');
-const asyncMiddleware = fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
+module.exports = app => {
+  // api here.
+  app.post('/api/target-user', jwtAuth, (req, res) => {
+    const userID = req.user._id;
+    const { id, login, avatar_url, html_url, events_url, url } = req.body;
 
-const saveUser = (userID, id, login, avatar_url, html_url, events_url, url) => {
-  User.findById(userID).then(user => {
-    if (!user._id.equals(userID)) {
-      throw new Error('Record sdfsdf exists');
-    }
-    TargetUser.findOne({ github_id: id })
-      .then(targetUser => {
-        if (targetUser) {
-          return new Error('whoops');
-        }
-        const newTargetUser = new TargetUser({
-          github_id: id,
-          login: login,
-          avatar_url: avatar_url,
-          html_url: html_url,
-          events_url: events_url,
-          url: url,
-          events_list: []
-        });
-        newTargetUser
-          .save()
-          .then(targetUser => targetUser.serialize())
-          .catch(err => {
-            console.log('IM DEAD HERE');
-            throw new Error(err);
+    User.findById(userID).then(user => {
+      if (!user._id.equals(userID)) {
+        return res.status(422).send({ error: 'Record already exists' });
+      }
+      TargetUser.findOne({ github_id: id })
+        .then(targetUser => {
+          console.log('targetUser: ', targetUser);
+          if (targetUser) {
+            return res.status(400).json({ error: 'Record already exists' });
+          }
+
+          const newTargetUser = new TargetUser({
+            github_id: id,
+            login: login,
+            avatar_url: avatar_url,
+            html_url: html_url,
+            events_url: events_url,
+            url: url,
+            events_list: []
           });
-      })
-      .catch(err => {
-        console.log('I DIE HERE');
-        next(err);
-      });
-
-    // console.log('This works!');
+          newTargetUser
+            .save()
+            .then(targetUser => res.send(targetUser))
+            .catch(err => {
+              res.status(400).send({ err });
+            });
+        })
+        .catch(err => {
+          res.status(400).send({ err });
+        });
+    });
   });
 };
 
-module.exports = app => {
-  // api here.
-  app.post(
-    '/api/target-user',
-    jwtAuth,
-    asyncMiddleware(async (req, res, next) => {
-      try {
-        const userID = req.user._id;
-        const gitHubToken = req.user.github_access_token;
-        // console.log(req.user);
-        setAuthToken(gitHubToken);
-        const { id, login, avatar_url, html_url, events_url, url } = req.body;
-        const user = await saveUser(
-          userID,
-          id,
-          login,
-          avatar_url,
-          html_url,
-          events_url,
-          url,
-          next
-        );
-        res.json(user);
-      } catch (error) {
-        next(error);
-      }
-    })
-  );
-};
+// app.get('/api/target-user', jwtAuth, (req, res) => {
+//   res.json({
+//     req
+//   });
+// })
